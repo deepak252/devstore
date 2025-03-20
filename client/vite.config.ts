b06efd4f54b5634d6104eafa94b5a8fbb4dc3936
deepak252 <deepakkudc16102000@gmail.com>
2025-03-21 00:02:24 +0530

    client: bug fix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,11 @@ import svgr from 'vite-plugin-svgr'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react(), svgr(), tsconfigPaths()],
-})
+  build: {
+    sourcemap: mode !== 'production', // generate sourcemaps only in dev/staging
+    minify: 'esbuild',
+    target: 'esnext',
+  },
+}))
