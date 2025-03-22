7cd7cdf6be8ed0b85501585fe4f6978e9064183d
deepak252 <deepakkudc16102000@gmail.com>
2025-03-22 14:04:38 +0530

    api-gateway: redis bug fix

--- services/api-gateway/src/config/redis.ts
+++ services/api-gateway/src/config/redis.ts
@@ -1,9 +1,10 @@
 import { Redis } from 'ioredis'
-import { REDIS_HOST, REDIS_PORT } from './env.js'
+import { REDIS_HOST, REDIS_PASSWORD, REDIS_PORT } from './env.js'
 import logger from '../utils/logger.js'
 
 const redisClient = new Redis({
   host: REDIS_HOST,
+  password: REDIS_PASSWORD,
   port: Number(REDIS_PORT),
   retryStrategy: () => null
 })
