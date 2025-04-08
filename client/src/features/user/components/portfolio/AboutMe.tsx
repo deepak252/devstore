5aeea56de6f597c6905a5f01a243db51f99c69bf
deepak252 <deepakkudc16102000@gmail.com>
2025-04-09 00:09:46 +0530

    UI fix

--- client/src/features/user/components/portfolio/AboutMe.tsx
+++ client/src/features/user/components/portfolio/AboutMe.tsx
@@ -5,14 +5,16 @@ const AboutMe = ({ about }: { about?: string }) => {
     <section id="about-me" className="mt-20">
       <h3 className="mb-10 max-md:text-2xl">About Me</h3>
       <div className="flex mt-12 gap-8 max-md:flex-col">
-        <div className="relative h-64 w-full min-w-64 max-md:mx-auto">
+        <div className="relative h-64 min-w-64 max-md:mx-auto max-md:w-full">
           <img
             src={CodeImg}
             alt="about"
             className="rounded-xl size-64 object-cover max-md:mx-auto max-md:w-full"
           />
         </div>
-        <p className="text-neutral-700 md:text-lg">{about}</p>
+        <p className="text-neutral-700 whitespace-pre-line md:text-lg">
+          {about}
+        </p>
       </div>
     </section>
   )
